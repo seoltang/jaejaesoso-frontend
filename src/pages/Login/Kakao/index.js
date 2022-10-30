da542018d78971226104a5bb8d76e95b9070ff22
석정도 <seogjeongdo@seogjeongdoui-MacBookPro.local>
2022-10-30 14:31:21 +0900

    기능: 소셜 로그인 cors 수정

--- src/pages/Login/Kakao/index.js
+++ src/pages/Login/Kakao/index.js
@@ -1,18 +1,28 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import qs from 'qs';
 
 const Kakao = () => {
   useEffect(() => {
     let params = new URL(window.location.href).searchParams;
     let code = params.get('code');
 
-    axios({
-      method: 'post',
-      url: 'localhost:3000/users/oauth/callback/kakao',
-      data: {
-        code,
-      },
+    let data = qs.stringify({
+      code: code,
     });
+
+    let config = {
+      method: 'post',
+      url: 'http://localhost:8000/users/oauth/callback/kakao',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data: data,
+    };
+
+    axios(config, { withCredentials: true })
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
   }, []);
 
   return <div>카카오 로그인 중</div>;
